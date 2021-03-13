d6959e98fc20c6497f24f62225fa720e32e98bb7
SovietModernism <basileuski@gmail.com>
2021-03-14 02:15:33 +0300

    added return to each function, added measure and compare functions

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,38 @@ import time
 import math
 
 
+# измерение скорости работы алгоритма
+def measure(func, n):
+    start = time.time()
+    func(n)
+    final = time.time() - start
+    
+    print ("Выполнение алгоритма занято", '{0:0.23f}'.format(final), "секунд.")
+    return final
+
+
+# измерение точности результата, сравнивая с pi из math
+def compare(func, n):
+    pi = func(n)
+    count = 0
+    
+    for i in range(15):
+        if str(pi)[i] == str(math.pi)[i]:
+            count += 1
+        else:
+            break
+    
+    if (count > 2):
+        print ("Точность составляет", count - 2, "знаков после запятой.")
+        return count - 2
+    elif (count == 2):
+        print ("Результат совпадает с Pi только в целой части.")
+        return 0
+    else:
+        print ("Число совсем не совпадает с Pi.")
+        return 0
+
+
 # ряд лейбница сходится очень медленно (8 после запятой при 1.000.000.000)
 def leibniz(n):
     pi = 0.0
@@ -14,7 +46,8 @@ def leibniz(n):
             pi -= 4 / x
         x += 2
 
-    print(pi)
+    print (pi)
+    return pi
 
 
 # ряд мадхавы является доработанной версией ряда лейбница и работает
@@ -32,7 +65,8 @@ def madhava(n):
 
     pi *= math.sqrt(12)
     
-    print(pi)
+    print (pi)
+    return pi
 
 
 # ряд нилаканта сложнее, но работает гораздо быстрее
@@ -47,7 +81,8 @@ def nilakantha(n):
             pi -= 4 / (base * (base+1) * (base+2))
         base += 2
 
-    print(pi)
+    print (pi)
+    return pi
 
 
 # формула виете по точности аналогична нилаканте
@@ -59,7 +94,10 @@ def viete(n):
        var = math.sqrt(2 + var)
        pi *= var / 2
 
-    print (2 / pi)
+    pi = 2 / pi
+
+    print (pi)
+    return pi
 
 
 # формула валлиса сходится очень медленно и по эффективности сходна с рядом лейбница
@@ -77,11 +115,16 @@ def wallis(n):
 
         pi *= x / y
     
-    print (pi * 2)
+    pi += 2
+    
+    print (pi)
+    return pi
 
 
 # формула мэчина
 def machin():
     pi = 4 * math.atan(1 / 5) - math.atan(1 / 239)
-    
-    print (pi * 4)
+    pi *= 4
+
+    print (pi)
+    return pi
